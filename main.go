80f0d044ab366fe422c2a2289da7c4e7903ef0dd
ouyangpeter <ouyangpeter911@gmail.com>
2016-03-08 18:41:15 +0800

    add mysql support,add mysql's Dockerfile

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	_ "property-management-system/routers"
-	"github.com/astaxie/beego"
+    _ "property-management-system/routers"
+    "github.com/astaxie/beego"
+    "github.com/astaxie/beego/orm"
+    "fmt"
+    _ "github.com/go-sql-driver/mysql"
 )
 
+func init() {
+    beego.BConfig.CopyRequestBody = true
+    beego.BConfig.WebConfig.Session.SessionOn = true
+    mysqlURL := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", beego.AppConfig.String("mysqluser"),
+        beego.AppConfig.String("mysqlpass"), beego.AppConfig.String("mysqlurls"), beego.AppConfig.String("mysqldb"))
+    orm.RegisterDataBase("default", "mysql", mysqlURL)
+}
+
 func main() {
-	beego.Run()
+    beego.Run()
 }
 
